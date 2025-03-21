4db3dfebc2c5c6d157a291bd15b1bed5149aeca2
onurabdulaji <onurabdulaji@gmail.com>
2025-03-21 01:24:33 +0100

    Db Extension Eklendi

---
+++ Infrastructure/AOPortfolioFull.Persistence/Extensions/DbContextExtension.cs
@@ -0,0 +1,17 @@
+﻿using AOPortfolioFull.Persistence.Context.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AOPortfolioFull.Persistence.Extensions;
+
+public static class DbContextExtension
+{
+    public static void AddDbContextExtension(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<AppDbContext>(options =>
+        {
+            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+        });
+    }
+}
